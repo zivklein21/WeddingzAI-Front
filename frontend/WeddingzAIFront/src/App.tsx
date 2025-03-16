3185096f3ee02c529134a42524243ebae9283226
gabimatatov <gavriel.matatov@veeva.com>
2025-03-16 16:56:53 +0200

    Poc frontend

---
+++ frontend/WeddingzAIFront/src/App.tsx
@@ -0,0 +1,11 @@
+import JsonUploader from "./components/JsonUploader/JsonUploader";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <JsonUploader />
+    </div>
+  );
+}
+
+export default App;
