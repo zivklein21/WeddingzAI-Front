2d54d3a001338476c8fad6ef7e6f0df37779721e
zivklein21 <zivklein21@gmail.com>
2025-04-02 23:05:21 +0300

    PROJ132-16-HomePage - create the home page - front

--- frontend/WeddingzAIFront/src/App.tsx
+++ frontend/WeddingzAIFront/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import PrefFormPage from "./pages/PrefFormPage";
 import TodoListPage from "./pages/TodoListPage";
+import HomePage from "./components/HomePage/HomePage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/home" element={<HomePage />} />
         <Route path="/" element={<PrefFormPage />} />
         <Route path="/todolist" element={<TodoListPage />} />
       </Routes>
