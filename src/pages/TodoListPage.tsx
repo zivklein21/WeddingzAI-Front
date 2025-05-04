0289672cdf620712c5414a8903ab548a48526cc2
gabimatatov <gavriel.matatov@veeva.com>
2025-05-04 23:11:29 +0300

    Added Protection for Pages

--- src/pages/TodoListPage.tsx
+++ src/pages/TodoListPage.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 import TodoList from "../components/TodoList/TodoList";
+import { useAuth } from "../hooks/useAuth/AuthContext";
+import { Navigate } from 'react-router-dom';
 
 const TodoListPage: React.FC = () => {
+
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/auth" />;
+  }
+
   return (
     <div>
       <TodoList />
