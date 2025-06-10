e33856a4f979a6e1a607bfe692c5245fef37d0f8
zivklein21 <zivklein21@gmail.com>
2025-06-10 20:38:47 +0300

    add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,27 @@ import AllVendors from "./components/Vendors/AllVendors";
 import InvitationPage from "./pages/InvitationPage";
 import CalendarPage from "./pages/CalendarPage";
 import MenuPage from "./pages/MenuPage";
+import { useEffect } from "react";
+import faviconSvg from "../src/assets/favicon.svg";
 
 
 export default function App() {
+
+     useEffect(() => {
+    document.title = "WeddingzAI";
+
+    // מציאת / יצירת תגית <link rel="icon">
+    let link: HTMLLinkElement | null = document.querySelector("link[rel*='icon']");
+    if (!link) {
+      link = document.createElement("link");
+      link.rel = "icon";
+      document.head.appendChild(link);
+    }
+    // הגדרת type ל־SVG ו-href לייבוא
+    link.type = "image/svg+xml";
+    link.href = faviconSvg;
+  }, []);
+
   return (
     <AuthProvider>
       <BrowserRouter>
